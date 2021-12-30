d623568b542c2dc122525faa5320c3dc876fa2fa
trubiso <trubiso@trubiso.localdomain>
2021-12-30 21:22:33 +0100

    fixed stuff

--- Token/Tokenizer.cpp
+++ Token/Tokenizer.cpp
@@ -16,19 +16,22 @@ std::vector<Token> Tokenizer::create_tokens() {
     std::vector<Token> tokens = std::vector<Token>();
     while(slice.size()) {
         Token token(TokenType::OTHER, "What");
-        bool foundToken = false;
-        std::cmatch cm;
+        bool found_token = false;
+        std::smatch sm;
+        std::string trimmed_slice = trim(slice);
 
         for (int i = 0 ; i < TOKEN_TYPE_QUANTITY ; i++) {
-            if(!std::regex_search(trim(slice).c_str(), cm, std::regex(TokenRegex[i]))) continue;
-            token = Token((TokenType) i, std::string(cm[0]));
+            std::regex current_regex = std::regex(TokenRegex[i]);
+            if(!std::regex_search(trimmed_slice, sm, current_regex, std::regex_constants::match_continuous)) continue;
+            std::string results = sm.str();
+            token = Token((TokenType) i, trim(results));
             tokens.push_back(token);
-            foundToken = true;
+            found_token = true;
             break;
         }
 
-        if (!foundToken) {
-            std::cout << "I hate myself (but not seriously :) )." << std::endl;
+        if (!found_token) {
+            std::cout << "I hate myself (but not seriously (or maybe seriously since I'm the program that's failing and shouldn't be plus I'm just a simple C++ program that is apparently self-conscious and is failing at such a simple task as tokenizing a piece of code...) :) )." << std::endl;
             return tokens;
         }
         advance(token.value.length());
